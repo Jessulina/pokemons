e3cda4a967d864e3ee69366df56e154b7ca43a66
Jessulina <133818678+Jessulina@users.noreply.github.com>
2024-08-28 11:19:13 -0300

    Add files via upload

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { useRoute, useRouter } from 'vue-router';
+
+
+    const route = useRoute();
+    const router = useRouter();
+
+    const backButton = () => {
+    router.push('/')
+}
+
+</script>
+
+
+
+<template>
+    <h1>404</h1>
+    <button @click="backButton" class="btn btn-outline-primary">volver</button>
+</template>
---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import {RouterLink} from 'vue-router';
+    import { useGetData } from '@/composables/getData';
+
+    const {data, getData, loading} = useGetData()
+    
+    getData('https://pokeapi.co/api/v2/pokemon')
+</script>
+
+<template>
+    <h1>Pokemons</h1>
+    <p v-if="loading">Cargando informacion...</p>
+    <div v-if="data">
+        <ul>
+            <li v-for="poke in data.results">
+                <router-link :to="`/pokemons/${poke.name}`">{{ poke.name }}</router-link>
+            </li>
+        </ul>
+    </div>
+    
+
+</template>
